78fcab8ad91f46fdb114b561066481019cac53fb
sofiaifoss <91983440+sofiaifoss@users.noreply.github.com>
2021-10-06 03:34:16 +0900

    Add files via upload

---
+++ precache-manifest.f211af883beabc382712c96ef8ae874e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0329ea8ddd2a767e3e80",
+    "url": "/css/app.d9f50950.css"
+  },
+  {
+    "revision": "336bed7b6f9fa7d667bc",
+    "url": "/css/chunk-vendors.7c48e540.css"
+  },
+  {
+    "revision": "fe94b77caa987d9d7ef7fbd68fc1f365",
+    "url": "/img/my.fe94b77c.png"
+  },
+  {
+    "revision": "d84aa68064eb336d0bab86f2f0eb9aed",
+    "url": "/img/situ.d84aa680.png"
+  },
+  {
+    "revision": "dc3504046f753f4773cd3803bd7b5b7a",
+    "url": "/img/weather.dc350404.png"
+  },
+  {
+    "revision": "8f29c739eb789bd4e87f5f30c4a10fae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0329ea8ddd2a767e3e80",
+    "url": "/js/app.c5513365.js"
+  },
+  {
+    "revision": "336bed7b6f9fa7d667bc",
+    "url": "/js/chunk-vendors.f87fcd92.js"
+  },
+  {
+    "revision": "c66c3a825e10b71505e4ee3c6029892a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
